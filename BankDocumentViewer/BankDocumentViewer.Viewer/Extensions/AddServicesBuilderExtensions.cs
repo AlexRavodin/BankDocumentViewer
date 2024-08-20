fabd29867e8723eb40a950ee454f621ef54c0dfa
Aleksandr Ravodin <91945858+Bel1eveme@users.noreply.github.com>
2024-08-20 15:01:03 +0300

    add hosting extensions

---
+++ BankDocumentViewer/BankDocumentViewer.Viewer/Extensions/AddServicesBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Viewer.Models;
+using Viewer.Services;
+
+namespace Viewer.Extensions;
+
+public static class AddServicesBuilderExtensions
+{
+    public static IHostBuilder AddServices(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices(services =>
+        {
+            services.AddSingleton<IFileService, FileService>();
+            services.AddSingleton<IDataGenerator, DataGenerator>();
+            services.AddSingleton<IDataProvider, DataProvider>();
+            services.AddSingleton<IBankDocumentParser, XlsBankDocumentParser>();
+        });
+
+        return hostBuilder;
+    }
+}
---
+++ BankDocumentViewer/BankDocumentViewer.Viewer/Extensions/AddViewModelsHostBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Viewer.Services;
+using Viewer.ViewModels;
+
+namespace Viewer.Extensions;
+
+public static class AddViewModelsHostBuilderExtensions
+{
+    public static IHostBuilder AddViewModels(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices(services =>
+        {
+            services.AddTransient<MenuViewModel>();
+            services.AddSingleton<Func<MenuViewModel>>(s => s.GetRequiredService<MenuViewModel>);
+            services.AddSingleton<NavigationService<MenuViewModel>>();
+
+            services.AddTransient<GenerateViewModel>();
+            services.AddSingleton<Func<GenerateViewModel>>(s => s.GetRequiredService<GenerateViewModel>);
+            services.AddSingleton<NavigationService<GenerateViewModel>>();
+
+            services.AddTransient(CreateFilesListViewModel);
+            services.AddSingleton<Func<FilesListViewModel>>(s => s.GetRequiredService<FilesListViewModel>);
+            services.AddSingleton<NavigationService<FilesListViewModel>>();
+
+            services.AddTransient(CreateOperationsListViewModel);
+            services.AddSingleton<Func<OperationsListViewModel>>(s => s.GetRequiredService<OperationsListViewModel>);
+            services.AddSingleton<NavigationService<OperationsListViewModel>>();
+
+            services.AddSingleton<MainViewModel>();
+        });
+
+        return hostBuilder;
+    }
+
+    private static FilesListViewModel CreateFilesListViewModel(IServiceProvider services)
+    {
+        return FilesListViewModel.LoadViewModel(services.GetRequiredService<NavigationService<MenuViewModel>>(),
+            services.GetRequiredService<NavigationService<OperationsListViewModel>>(),
+            services.GetRequiredService<IDataProvider>(), 
+            services.GetRequiredService<IBankDocumentParser>());
+    }
+
+    private static OperationsListViewModel CreateOperationsListViewModel(IServiceProvider services)
+    {
+        return OperationsListViewModel.LoadViewModel(
+            services.GetRequiredService<NavigationService<FilesListViewModel>>(),
+            services.GetRequiredService<IDataProvider>());
+    }
+}
